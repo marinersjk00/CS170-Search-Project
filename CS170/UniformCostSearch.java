c065d2a9dec4eff95f76e32e2725a51a24bb0692
Jordan Kuschner <jkusc002@ucr.edu>
2022-10-29 18:50:00 -0700

    Swapping not working properly

--- CS170/UniformCostSearch.java
+++ CS170/UniformCostSearch.java
@@ -31,10 +31,28 @@ static class State{
         }
   }
   
+  State(ArrayList<ArrayList<Integer>> p, int d, int i, int j){
+      this.puzzle = p;
+      this.depth = d;
+      this.emptyI = i;
+      this.emptyJ = j;
+}
+  
   
 
 }
 
+static class StateCompare implements Comparator<State>{
+	public int compare(State s, State p) {
+		if(s.depth < p.depth) {
+			return 1;
+		}else if(s.depth > p.depth) {
+			return -1;
+		}else {
+			return 0;
+		}
+	}
+}
 
 
 
@@ -53,28 +71,79 @@ static class State{
 		
 	}
 
-	/*static boolean isGoal(ArrayList<ArrayList<Integer>> puzzle, ArrayList<ArrayList<Integer>> goal){
+	static boolean isGoal(ArrayList<ArrayList<Integer>> puzzle, ArrayList<ArrayList<Integer>> goal){
 		for(int i = 0; i < puzzleDim; i++){
 			for(int j = 0; j < puzzleDim; j++){
 				if(puzzle.get(i).get(j) != goal.get(i).get(j)) return false;
 			}
 		}
 		
+		
+		System.out.print("\n\n");
+		for(int i = 0; i < puzzleDim; i++) {
+			for(int j = 0; j < puzzleDim; j++) {
+				System.out.print(puzzle.get(i).get(j) + "  ");
+			}
+			System.out.println();
+		}
+		
 		return true;
 	}
 	
 	
-	static void search(State curr, HashMap<ArrayList<ArrayList<Integer>>, Integer> seen) {
+	static void search(State curr, HashMap<ArrayList<ArrayList<Integer>>, Integer> seen, Queue<State> next) {
 		
 		if(curr.emptyI < puzzleDim - 1) {
-			State nextState = new State()
+			State nextState = new State(swap(curr.puzzle, 0, curr.puzzle.get(curr.emptyI + 1).get(curr.emptyJ), curr.emptyI, curr.emptyJ, curr.emptyI + 1, curr.emptyJ), curr.depth + 1, curr.emptyI + 1, curr.emptyJ);
+			if(!seen.containsKey(nextState.puzzle)) {
+				seen.put(nextState.puzzle, nextState.depth);
+				//System.out.print("Q Check");
+				next.add(nextState);
+				
+			}
 		}
 		
-	}*/
+		if(curr.emptyI > 0) {
+			State nextState = new State(swap(curr.puzzle, 0, curr.puzzle.get(curr.emptyI - 1).get(curr.emptyJ), curr.emptyI, curr.emptyJ, curr.emptyI - 1, curr.emptyJ), curr.depth + 1, curr.emptyI - 1, curr.emptyJ);
+			if(!seen.containsKey(nextState.puzzle)) {
+				seen.put(nextState.puzzle, nextState.depth);
+				//System.out.print("Q Check");
+
+				next.add(nextState);
+			}
+		}
+		
+		if(curr.emptyJ < puzzleDim - 1) {
+			State nextState = new State(swap(curr.puzzle, 0, curr.puzzle.get(curr.emptyI).get(curr.emptyJ + 1), curr.emptyI, curr.emptyJ, curr.emptyI, curr.emptyJ + 1), curr.depth + 1, curr.emptyI, curr.emptyJ + 1);
+			if(!seen.containsKey(nextState.puzzle)) {
+				seen.put(nextState.puzzle, nextState.depth);
+				//System.out.print("Q Check");
+
+				next.add(nextState);
+				
+			}
+		}
+		
+		if(curr.emptyJ > 0) {
+			State nextState = new State(swap(curr.puzzle, 0, curr.puzzle.get(curr.emptyI).get(curr.emptyJ - 1), curr.emptyI, curr.emptyJ, curr.emptyI, curr.emptyJ - 1), curr.depth + 1, curr.emptyI, curr.emptyJ - 1);
+			if(!seen.containsKey(nextState.puzzle)) {
+				seen.put(nextState.puzzle, nextState.depth);
+				//System.out.print("Q Check");
+
+				next.add(nextState);
+				
+			}
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		ArrayList<ArrayList<Integer>>  puzzle = new ArrayList<ArrayList<Integer>>() ;
 		
 		ArrayList<ArrayList<Integer>>  solved = new ArrayList<ArrayList<Integer>>() ;
+		HashMap<ArrayList<ArrayList<Integer>>, Integer> seen = new HashMap<>();	
+		PriorityQueue<State> next = new PriorityQueue<State>(new StateCompare());
+		
 		int goalNum = 1;
 		for(int i = 0; i < puzzleDim; i++){
 			
@@ -100,8 +169,7 @@ static class State{
 		
 		
 		
-		HashMap<ArrayList<ArrayList<Integer>>, Integer> seen = new HashMap<>();	
-		Queue<ArrayList<ArrayList<Integer>>> next = new LinkedList<>();
+		
 		
 		System.out.print("Please enter the puzzle start from L to R, Top to Bottom. Enter '0' to represent the empty space\n\n");
 	for(int i = 0; i < puzzleDim; i++){
@@ -120,26 +188,36 @@ static class State{
 		}
 		System.out.println();
 	}
-				
-	puzzle = swap(puzzle, puzzle.get(0).get(0), puzzle.get(0).get(1), 0, 0, 0, 1);
+		System.out.println();
+		State start = new State(puzzle, 0);
+		
+		
+		
+		next.add(start);
+		
+		while(!next.isEmpty()) {
+			State curr = next.remove();
+			System.out.print("Depth: " + curr.depth + "\n");
+			System.out.print("\nState:\n\n");
+			for(int i = 0; i < puzzleDim; i++) {
+				for(int j = 0; j < puzzleDim; j++) {
+					System.out.print(curr.puzzle.get(i).get(j) + "  " );
+				}
+				System.out.println();
+			}
+			
+			if(isGoal(curr.puzzle, solved)) break;
+			
+			search(curr, seen, next);		
+		}
+		
+		
+		
 	
 
 
-	System.out.println("\n\nSwapped puzzle: ");
-	for(int i = 0; i < puzzleDim; i++){
-		for(int j = 0; j < puzzleDim; j++){
-			System.out.print(puzzle.get(i).get(j) + "  ");
-		}
-		System.out.println();
-	}
-System.out.println("\n\n");
-		for(int i = 0; i < puzzleDim; i++){
-			for(int j = 0; j < puzzleDim; j++){
-				System.out.print(puzzle.get(i).get(j) + "  ");
-	}
-				System.out.println();
+	
 
-	}
 		
 
 
